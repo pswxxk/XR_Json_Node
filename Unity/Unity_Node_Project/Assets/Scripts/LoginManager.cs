1313d975a23c4acbd6a3c88fce09a2362dc37fb0
pswxxk <sangwook2001@gmail.com>
2023-09-27 15:32:09 +0900

    Feat : "보호된 토큰 전송"

--- Unity/Unity_Node_Project/Assets/Scripts/LoginManager.cs
+++ Unity/Unity_Node_Project/Assets/Scripts/LoginManager.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 
 public class LoginManager : MonoBehaviour
 {
-    public InputField usernameInput;            //유저 필드
-    public InputField passwordInput;            //패스워드 필드
+    public InputField usernameInput;        //유저 필드
+    public InputField passwordInput;        //패스워드 필드
     public Text loginStatuText;
 
     public string token;
@@ -19,18 +19,17 @@ public class LoginManager : MonoBehaviour
     {
         StartCoroutine(AttemptLogin(usernameInput.text, passwordInput.text));
     }
-
     IEnumerator AttemptLogin(string username, string password)
     {
         WWWForm form = new WWWForm();
         form.AddField("username", username);
-        form.AddField("password", password);    
+        form.AddField("password", password);
 
-        using (UnityWebRequest webRequest = UnityWebRequest.Post(apiUrl + "/login", form)) 
-        { 
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(apiUrl + "/login", form))
+        {
             yield return webRequest.SendWebRequest();
-            
-            if(webRequest.result != UnityWebRequest.Result.Success) 
+
+            if (webRequest.result != UnityWebRequest.Result.Success)
             {
                 loginStatuText.text = "Login failed : " + webRequest.error;
             }
@@ -38,7 +37,7 @@ public class LoginManager : MonoBehaviour
             {
                 loginStatuText.text = "Login successful!";
                 string responseText = webRequest.downloadHandler.text;
-                //Json 응답에서 토큰 값을 추출
+                //JSON 응답에서 토큰 값을 추출
                 var responseData = JsonConvert.DeserializeObject<ResponseData>(responseText);
                 token = responseData.token;
 
@@ -47,10 +46,48 @@ public class LoginManager : MonoBehaviour
         }
     }
 
-    [System.Serializable]
+    public void SendAutienticatedRequest(string endpoint)
+    {
+        if (string.IsNullOrEmpty(token))
+        {
+            Debug.LogError("Token is missing");
+            return;
+        }
 
+        UnityWebRequest www = UnityWebRequest.Get(apiUrl + endpoint);
+        www.SetRequestHeader("Authorization", token);
+
+        StartCoroutine(SendRequest(www));
+    }
+    IEnumerator SendRequest(UnityWebRequest webRequest)
+    {
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.result == UnityWebRequest.Result.ConnectionError ||
+            webRequest.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log("서버 통신 에러 " + webRequest.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log("Request successful" + webRequest.downloadHandler.text);
+        }
+    }
+
+    public void SendAuthenticatedRequestToProtectedEndpoin()
+    {
+        if (string.IsNullOrEmpty(token))
+        {
+            Debug.LogError("Token is missing");
+            return;
+        }
+
+        SendAutienticatedRequest("/protected");
+    }
+
+    [System.Serializable]
     private class ResponseData
     {
-        public string token;    
+        public string token;
     }
 }
